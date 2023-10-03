4f67e420841e5730287c2ea7980d0af2027c20d9
Alex Sieke <asieke@gmail.com>
2023-10-03 17:06:36 -0400

    console logging payload

--- src/routes/api/chat/webhook/+server.ts
+++ src/routes/api/chat/webhook/+server.ts
@@ -1,4 +1,8 @@
-export const GET = async () => {
+export const GET = async ({ request }: { request: Request }) => {
+	const payload = await request.json();
+
+	console.log('payload', payload);
 	console.log('running webhook.... hello!');
+
 	return new Response(JSON.stringify({ bones: 'money' }));
 };
