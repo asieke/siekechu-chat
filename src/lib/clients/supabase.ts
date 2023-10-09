39adda362c3ac7b73a99cecc04c7a83c7c8b843a
Alex Sieke <asieke@gmail.com>
2023-10-09 11:48:55 -0400

    updating secret nomenclature

--- src/lib/clients/supabase.ts
+++ src/lib/clients/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SERVICE_ROLE_KEY } from '$env/static/public';
+import { SUPABASE_URL, SUPABASE_KEY } from '$env/static/private';
 import type { Database } from './database';
 
-export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SERVICE_ROLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
 	auth: { persistSession: false }
 });
